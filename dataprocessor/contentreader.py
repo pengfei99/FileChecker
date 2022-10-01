8a780b8ce9be16b75d402511cedce181880c23d1
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-01 18:56:17 +0200

    add read csv

--- dataprocessor/contentreader.py
+++ dataprocessor/contentreader.py
@@ -1,5 +1,11 @@
+import pandas as pd
 
-def read_txt_file(file_path,encoding='utf8'):
+
+def read_txt_file(file_path: str, encoding='utf8'):
     with open(file_path, encoding=encoding) as f:
         for line in f:
             yield line.strip()
+
+
+def read_csv_file(file_path, sep=",", delimiter=None, header=0):
+    return pd.read_csv(file_path, sep=sep, delimiter=delimiter, header=header)
