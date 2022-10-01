5d65abb9d859ca0a57303fc74f64f1b6d79fbb88
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-01 18:17:55 +0200

    add unit test

---
+++ tests/test_directorywalker.py
@@ -0,0 +1,7 @@
+from directorywalker.directorywalker import list_files
+
+
+def test_list_files():
+    path_dir = "/home/pliu/git/FileChecker"
+    file_list = list_files(path_dir)
+    print(file_list)
