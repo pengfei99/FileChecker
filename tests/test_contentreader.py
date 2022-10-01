28d0602e3d520ac826cef663b83990f53453aa74
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-01 18:56:39 +0200

    add unit test for read_csv_file

--- tests/test_contentreader.py
+++ tests/test_contentreader.py
@@ -1,8 +1,22 @@
-from dataprocessor.contentreader import read_txt_file
+from dataprocessor.contentreader import read_txt_file, read_csv_file
+
+root_path = "/home/pliu/git/FileChecker/data"
 
 
 def test_read_txt_file():
-    file_path = "/home/pliu/git/FileChecker/data/test.txt"
+    file_path = f"{root_path}/test.txt"
     lines = read_txt_file(file_path)
     for line in lines:
         print(line)
+
+
+def test_read_csv_file_with_header():
+    file_path = f"{root_path}/with_header.csv"
+    df = read_csv_file(file_path)
+    print(df.head())
+
+
+def test_read_csv_file_without_header():
+    file_path = f"{root_path}/without_header.csv"
+    df = read_csv_file(file_path, header=None)
+    print(df.head())
