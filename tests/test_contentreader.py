1c6a11867974d5225c94a89305a44d4fd3034c38
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-02 16:10:34 +0200

    tests/

--- tests/test_contentreader.py
+++ tests/test_contentreader.py
@@ -1,4 +1,4 @@
-from dataprocessor.contentreader import read_txt_file, read_csv_file, read_pdf_file
+from dataprocessor.contentreader import read_txt_file, read_csv_file, read_pdf_file, read_excel_file
 
 root_path = "/home/pliu/git/FileChecker/data"
 
@@ -25,9 +25,15 @@ def test_read_csv_file_without_header():
 def test_read_pdf_file():
     file_path = f"{root_path}/file-sample.pdf"
     page_nums, texts = zip(*read_pdf_file(file_path))
-    for i in range(0,len(page_nums)):
+    for i in range(0, len(page_nums)):
         print(f"At page {page_nums[i]}:")
         print(f"content: {texts[i]}")
 
 
-
+def test_read_excel_file():
+    file_path = f"{root_path}/sample_data.xlsx"
+    df_dir = read_excel_file(file_path)
+    print(type(df_dir))
+    for key in df_dir:
+        print(f"sheet_name: {key}")
+        print(df_dir[key].head())
