609f7e13ff2b73e52f68ba58598e99bf2a7e80d1
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-02 15:55:40 +0200

    add pdf reader

--- dataprocessor/contentreader.py
+++ dataprocessor/contentreader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from PyPDF2 import PdfReader
 
 
 def read_txt_file(file_path: str, encoding='utf8'):
@@ -9,3 +10,12 @@ def read_txt_file(file_path: str, encoding='utf8'):
 
 def read_csv_file(file_path, sep=",", delimiter=None, header=0):
     return pd.read_csv(file_path, sep=sep, delimiter=delimiter, header=header)
+
+
+def read_pdf_file(file_path: str):
+    reader = PdfReader(file_path)
+    number_of_pages = len(reader.pages)
+
+    for i in range(0, number_of_pages):
+        page = reader.pages[i]
+        yield i, page.extract_text()
