49b1af4122e7873796119b46e53923df985fb0c5
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-10-02 15:56:02 +0200

    add unit test for pdf reader

--- tests/test_contentreader.py
+++ tests/test_contentreader.py
@@ -1,4 +1,4 @@
-from dataprocessor.contentreader import read_txt_file, read_csv_file
+from dataprocessor.contentreader import read_txt_file, read_csv_file, read_pdf_file
 
 root_path = "/home/pliu/git/FileChecker/data"
 
@@ -20,3 +20,14 @@ def test_read_csv_file_without_header():
     file_path = f"{root_path}/without_header.csv"
     df = read_csv_file(file_path, header=None)
     print(df.head())
+
+
+def test_read_pdf_file():
+    file_path = f"{root_path}/file-sample.pdf"
+    page_nums, texts = zip(*read_pdf_file(file_path))
+    for i in range(0,len(page_nums)):
+        print(f"At page {page_nums[i]}:")
+        print(f"content: {texts[i]}")
+
+
+
